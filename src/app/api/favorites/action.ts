802930cee18207c8cee00d3c68d5657762f27d5b
daniel9212 <daniel.bezedea@gmail.com>
2024-11-30 20:25:02 +0200

    Adds random favorite cities and other random cities on the Home page.

--- src/app/api/favorites/action.ts
+++ src/app/api/favorites/action.ts
@@ -1,15 +1,33 @@
 import appDataSourceInitialization from '@/app/db/connection';
 import { City } from '@/app/db/entities/City';
 
-export const getFavoriteCities = async (userId: string) => {
+export const getFavoriteCities = async (
+  userId: string,
+  {
+    limit,
+    orderBy,
+  }: {
+    limit?: number;
+    orderBy?: string;
+  } = {},
+) => {
   const dataSource = await appDataSourceInitialization;
 
-  const favoriteCities = await dataSource
+  let favoriteCitiesQuery = dataSource
     .getRepository(City)
     .createQueryBuilder('city')
     .innerJoin('city.users', 'user')
-    .where('user.id = :userId', { userId })
-    .getMany();
+    .where('user.id = :userId', { userId });
+
+  if (orderBy) {
+    favoriteCitiesQuery = favoriteCitiesQuery.orderBy(orderBy);
+  }
+
+  if (limit) {
+    favoriteCitiesQuery = favoriteCitiesQuery.limit(limit);
+  }
+
+  const favoriteCities = await favoriteCitiesQuery.getMany();
 
   return {
     data: { favoriteCities },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,84 @@
-export default function Home() {
+import Link from 'next/link';
+import { Grid, GridItem, Stack, Card } from '@chakra-ui/react';
+import { redirect } from 'next/navigation';
+
+import { auth } from '@root/auth';
+import NavButton from '@/app/navigation/nav-button';
+import FavoriteCitiesCardBody from '@/app/favorites/favorite-cities-card-body';
+import { getRandomCities } from '@/app/api/home/action';
+import { getFavoriteCities } from '@/app/api/favorites/action';
+
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    redirect('/account/login');
+  }
+
+  const randomCities = await getRandomCities(5, 3);
+
+  const {
+    user: { id: userId },
+  } = session;
+
+  const {
+    data: { favoriteCities },
+  } = await getFavoriteCities(userId, {
+    limit: 5,
+    orderBy: 'RANDOM()',
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
-    </div>
+    <Grid h="full" templateColumns="repeat(4, 1fr)" gap="6">
+      <GridItem colSpan={1} />
+      <GridItem
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        colSpan={2}
+      >
+        <Card.Root w="full" h="23.5rem" p="6" variant="subtle">
+          <FavoriteCitiesCardBody favoriteCities={favoriteCities} />
+        </Card.Root>
+        <NavButton
+          mt="28"
+          fontSize="lg"
+          fontWeight="bold"
+          p="6"
+          bgColor="red.700"
+          text="Explore Cities"
+          href="/search"
+        />
+      </GridItem>
+      <GridItem
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        colSpan={1}
+      >
+        <Stack w="full">
+          {randomCities.map(({ id, name, country, latitude, longitude }) => (
+            <Card.Root key={id}>
+              <Link
+                href={{
+                  pathname: `/cities/${name}`,
+                  query: {
+                    country,
+                    latitude,
+                    longitude,
+                  },
+                }}
+              >
+                <Card.Body p="4" gap="2">
+                  <Card.Title>{name}</Card.Title>
+                  <Card.Description>{country}</Card.Description>
+                </Card.Body>
+              </Link>
+            </Card.Root>
+          ))}
+        </Stack>
+      </GridItem>
+    </Grid>
   );
 }
