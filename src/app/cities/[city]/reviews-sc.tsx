ec22ae222f69aadbaa9a016273a5482ccab7d130
daniel9212 <daniel.bezedea@gmail.com>
2024-12-11 11:19:16 +0200

    Adds permissions and adds some refactoring in City page.

---
+++ src/app/cities/[city]/reviews-sc.tsx
@@ -0,0 +1,22 @@
+import type { ReviewsProps } from '@/app/cities/[city]/reviews';
+
+import {
+  type ReviewWithSanitizedUser,
+  fetchReviews,
+} from '@/app/api/cities/[city]/action';
+import Reviews from '@/app/cities/[city]/reviews';
+
+export default async function ReviewsSC({
+  cityData,
+  user,
+}: Omit<ReviewsProps, 'defaultReviews'>) {
+  let reviews = [] as ReviewWithSanitizedUser[];
+
+  try {
+    reviews = await fetchReviews(cityData);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return <Reviews user={user} defaultReviews={reviews} cityData={cityData} />;
+}
