961f97a68946df9e02fbde784db8465b9d3eda17
daniel9212 <daniel.bezedea@gmail.com>
2024-12-11 09:47:39 +0200

    Removes Favorites button from navbar if user is not logged in.

--- src/app/navigation/navigation.tsx
+++ src/app/navigation/navigation.tsx
@@ -1,18 +1,19 @@
 import { Box, HStack } from '@chakra-ui/react';
 
 import LogOutButton from '@/app/navigation/logout-button';
-
 import { ColorModeButton } from '@/app/components/color-mode';
 import NavButton from '@/app/navigation/nav-button';
+import { auth } from '@root/auth';
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
   return (
     <Box bg="gray.300">
       <HStack as="nav" mx="auto" py="2.5" w="9/12" gap="6" top="0">
         <Box mr="auto">Logo</Box>
         <NavButton text="Home" href="/" />
         <NavButton text="Search" href="/search" />
-        <NavButton text="Favorites" href="/favorites" />
+        {session && <NavButton text="Favorites" href="/favorites" />}
         <LogOutButton />
         <ColorModeButton />
       </HStack>
