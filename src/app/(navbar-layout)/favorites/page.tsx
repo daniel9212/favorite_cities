6fadfa635ceef19eb85f0f2b4cfca2aa8008b2d1
daniel9212 <daniel.bezedea@gmail.com>
2025-01-02 11:07:13 +0200

    Removes unnecessary FavoriteCitiesCardBody.

--- src/app/(navbar-layout)/favorites/page.tsx
+++ src/app/(navbar-layout)/favorites/page.tsx
@@ -1,6 +1,7 @@
-import { Center, Card } from '@chakra-ui/react';
+import { Center, Card, Table } from '@chakra-ui/react';
 
-import FavoriteCitiesCardBody from '@/app/(navbar-layout)/favorites/favorite-cities-card-body';
+import NavButton from '@/app/navigation/nav-button';
+import { getFavoriteCities } from '@/app/api/favorites/action';
 import { auth } from '@root/auth';
 import { redirect } from 'next/navigation';
 
@@ -15,13 +16,50 @@ export default async function Favorites() {
     user: { id: userId },
   } = session;
 
+  const {
+    data: { favoriteCities },
+  } = await getFavoriteCities(userId);
+
+  const hasFavoriteCities = !!favoriteCities.length;
   return (
     <Center h="full">
       <Card.Root h="50vh" p="6" w="1/2" variant="subtle">
         <Card.Header fontSize="3xl" fontWeight="bold" p="0" pb="6">
           Favorite Cities
         </Card.Header>
-        <FavoriteCitiesCardBody userId={userId} />
+        <Card.Body px="0" py="0" overflow="auto">
+          {hasFavoriteCities && (
+            <Table.Root>
+              <Table.Body>
+                {favoriteCities.map(
+                  ({ id, name, country, latitude, longitude }) => (
+                    <Table.Row key={id}>
+                      <Table.Cell>{name}</Table.Cell>
+                      <Table.Cell>{country}</Table.Cell>
+                      <Table.Cell textAlign="end">
+                        <NavButton
+                          colorPalette="cyan"
+                          text="View"
+                          href={{
+                            pathname: `/cities/${name}`,
+                            query: {
+                              country,
+                              latitude,
+                              longitude,
+                            },
+                          }}
+                        />
+                      </Table.Cell>
+                    </Table.Row>
+                  ),
+                )}
+              </Table.Body>
+            </Table.Root>
+          )}
+          {!hasFavoriteCities && (
+            <Center h="full">Nothing to see here yet!</Center>
+          )}
+        </Card.Body>
       </Card.Root>
     </Center>
   );
